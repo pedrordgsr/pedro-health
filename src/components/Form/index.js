d49e4575849d7a145ad823c31ee9a137c25e3fc1
Pedro R. <pedrorodriguesssrrr@gmail.com>
2024-07-26 18:00:12 -0300

    Aula 2 - PT 1

---
+++ src/components/Form/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View,Text,TextInput, Button } from "react-native";
+import ResultImc from "../ResultIMC";
+
+export default function Form(){
+    return (
+        <View>
+            <View>
+                
+                <Text>Altura</Text>
+                <TextInput
+                placeholder = "Ex. 1.75"
+                keyboardType = "numeric"   
+                />
+                
+                <Text>Peso</Text>
+                <TextInput
+                placeholder = "Ex. 75.365"
+                keyboardType = "numeric"   
+                />
+
+                <Button title="Calcular IMC"/>
+            </View>
+            <ResultImc messageResultImc={messageImc} resultImc={imc}/>
+        </View>
+    )
+}
