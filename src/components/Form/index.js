58b0a4987e686a7e3f6b8af49438963e3a252279
Pedro R. <pedrorodriguesssrrr@gmail.com>
2024-07-31 17:47:52 -0300

    funcional

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,27 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { View,Text,TextInput, Button } from "react-native";
-import ResultImc from "../ResultIMC";
+import ResultImc from "../ResultImc";
 
 export default function Form(){
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [messageImc, setMessageImc] = useState("Preencha o peso e a altura");
+    const [imc,setImc] = useState(null);
+    const [textButton, setTextButton] = useState("Calcular IMC");''
+
+    function imcCalculator(){
+        return setImc((weight/(height*height)).toFixed(2));
+    }
+
+    function validationImc(){
+        if(weight != null && height != null){
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu IMC é igual a ")
+            setTextButton("Calcular novamente")
+            return
+        }
+        setImc(null)
+        setTextButton("Calcular")
+    }
+
+
     return (
         <View>
             <View>
                 
                 <Text>Altura</Text>
                 <TextInput
+                onChangeText = {setHeight}
+                value = {height}
                 placeholder = "Ex. 1.75"
                 keyboardType = "numeric"   
                 />
                 
                 <Text>Peso</Text>
                 <TextInput
+                onChangeText = {setWeight}
+                value = {weight}
                 placeholder = "Ex. 75.365"
                 keyboardType = "numeric"   
                 />
 
-                <Button title="Calcular IMC"/>
+                <Button 
+                onPress = {() => validationImc()}
+                title="Calcular IMC"
+                />
             </View>
-            <ResultImc messageResultImc={messageImc} resultImc={imc}/>
+            <View>{height}</View>
+            <View>{weight}</View>
+            <ResultImc 
+            messageResultImc={messageImc} 
+            resultImc={imc}/>
         </View>
     )
 }
--- src/components/ResultImc/index.js
+++ src/components/ResultImc/index.js
@@ -4,8 +4,8 @@ import { View,Text,TextInput, Button } from "react-native";
 export default function ResultImc(props){
     return (
         <View>
-            <Text>{props.ResultImc}</Text>
             <Text>{props.messageResultImc}</Text>
+            <Text>{props.resultImc}</Text>
         </View>
     )
 }
